b6f40a7352853d23966fd6f5fe3ba1e96016f065
sijandahal <dahalsijan7@gmail.com>
2024-11-08 21:38:40 -0600

    added key on the showcase

--- src/slices/Bento/index.tsx
+++ src/slices/Bento/index.tsx
@@ -43,8 +43,8 @@ const Bento = ({ slice }: BentoProps): JSX.Element => {
       </div>
 
       <div className="mt-16 grid max-w-4xl grid-rows-[auto_auto_auto] gap-8 md:grid-cols-3 md:gap-10">
-        {slice.primary.repeatable_zone.map((item) => (
-            <div
+        {slice.primary.repeatable_zone.map((item, index) => (
+            <div key={index}
             className={clsx(
               "glass-container row-span-3 grid grid-rows-subgrid gap-4 rounded-lg bg-gradient-to-b from-gray-900 to-gray-950 p-4",
               item.wide ? "md:col-span-2" : "md:col-span-1",
